9695fc77ba5852e1232f9125310398c79aeb2d62
milestarricone <milestarricone2@gmail.com>
2024-10-29 17:48:05 -0400

    second commit, Added  functionality and credits functionality. next is the start game function

--- JS/app.js
+++ JS/app.js
@@ -1,16 +1,42 @@
 // consts
 
-const quitButton = document.getElementById(`quitButton`)
-
+const quitButtonElement = document.getElementById(`quitButton`)
+const menuContainerElement = document.getElementById(`mainMenu`)
+const bodyElement = document.getElementById(`body`)
+const creditsButtonElement = document.getElementById(`creditsButton`)
 
 // variables========================================================================================================
 
 
 
 // tests==========================================================================================
-console.log(quitButton)
-
-
+console.dir(quitButtonElement)
+console.dir(menuContainerElement)
+console.dir(bodyElement)
+console.dir(creditsButtonElement)
 
 
 // Functions=======================================================================================================
+
+quitGame = () => { ////////should close the game window//// Tested MT
+    window.close()
+}
+credits = () => {
+    const devs = document.createElement(`h1`)
+    devs.innerHTML = `Miles Tarricone`
+    menuContainerElement.appendChild(devs)
+}
+
+
+//event handlera==================================================================================================================
+
+quitButtonElement.addEventListener(`click`, () => {
+    quitGame()
+})
+creditsButtonElement.addEventListener(`click`, () => {
+    credits()
+})
+
+
+//Dead code==================================================================================
+// quitGame() tested
